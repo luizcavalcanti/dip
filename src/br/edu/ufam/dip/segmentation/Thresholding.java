835f3e4a099b6799fb9cd0a20e6a2f0ea131646f
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-06-07 12:40:17 -0400

    Added interval on threshold segmentation
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/br/edu/ufam/dip/segmentation/Thresholding.java
+++ src/br/edu/ufam/dip/segmentation/Thresholding.java
@@ -13,19 +13,32 @@ public class Thresholding {
 
     public static void main(String[] args) throws IOException {
         if (args.length != 3) {
-            System.err.println("Usage: Thresholding <source-image> <dest-image> <threshold>");
+            System.err.println("Usage: Thresholding <source-image> <dest-image> <threshold or interval>");
+            System.err.println("example:");
+            System.err.println("          Thresholding source.png dest.png 85");
+            System.err.println(" or       Thresholding source.png dest.png 150:220");
             System.exit(1);
         }
         String origPath = args[0];
         String destPath = args[1];
-        int threshold = Integer.parseInt(args[2]);
+        int threshold1;
+        int threshold2;
+        if (args[2].indexOf(':')>-1) {
+            String[] ts = args[2].split(":");
+            threshold1 = Integer.parseInt(ts[0]);
+            threshold2 = Integer.parseInt(ts[1]);
+        } else {
+            threshold1 = Integer.parseInt(args[2]);
+            threshold2 = -1;
+        }
+
         BufferedImage image = ImageIOUtils.loadImageFromFile(origPath);
         int[][] imageData = ImageIOUtils.getImageData(image);
-        int[][] output = applySegmentation(imageData, threshold);
+        int[][] output = applySegmentation(imageData, threshold1, threshold2);
         ImageIO.write(ImageIOUtils.getImageFromData(output), "png", new File(destPath));
     }
 
-    private static int[][] applySegmentation(int[][] imageData, int threshold) {
+    public static int[][] applySegmentation(int[][] imageData, int threshold1, int threshold2) {
         int width = imageData.length;
         int height = imageData[0].length;
         int[][] output = new int[width][height];
@@ -33,8 +46,12 @@ public class Thresholding {
             for (int y = 0; y < height; y++) {
                 Color c = new Color(imageData[x][y]);
                 int grey = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
-                if (grey > threshold) {
-                    output[x][y] = new Color(255, 255, 255).getRGB();
+                if (threshold2==-1) {
+                    if (grey > threshold1) {
+                        output[x][y] = new Color(255, 255, 255).getRGB();
+                    }
+                } else if (grey > threshold1 && grey < threshold2) {
+                        output[x][y] = new Color(255, 255, 255).getRGB();
                 }
             }
         }
