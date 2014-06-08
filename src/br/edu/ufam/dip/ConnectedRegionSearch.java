2d0de8e62c7c66ac460fe452ee5eea70b2d31cc0
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-06-08 17:50:59 -0400

    Refactor on connected region search to work standalone
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/br/edu/ufam/dip/ConnectedRegionSearch.java
+++ src/br/edu/ufam/dip/ConnectedRegionSearch.java
@@ -1,17 +1,44 @@
-package br.edu.ufam;
+package br.edu.ufam.dip;
 
 import java.awt.Color;
 import java.awt.Point;
 import java.util.HashSet;
 import java.util.Set;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import br.edu.ufam.ImageIOUtils;
 
 public class ConnectedRegionSearch {
 
-    public static Set<Point> extractConnectedRegion(int[][] imageData, Point origin, int threshold) {
-        return searchForConnectedNeighbors(imageData, origin, threshold);
+    public static void main(String[] args) throws IOException {
+        if (args.length != 5) {
+            System.err.println("Usage: ConnectedRegionSearch <source-image> <dest-image> startX startY threshold");
+            System.err.println("example:");
+            System.err.println("          ConnectedRegionSearch source.png dest.png 12 45 30");
+            System.exit(1);
+        }
+        String origPath = args[0];
+        String destPath = args[1];
+        int x = Integer.parseInt(args[2]);
+        int y = Integer.parseInt(args[3]);
+        int threshold = Integer.parseInt(args[4]);
+
+        BufferedImage image = ImageIOUtils.loadImageFromFile(origPath);
+        int[][] imageData = ImageIOUtils.getImageData(image);
+        int[][] output = new int[imageData.length][imageData[0].length];
+
+        Set<Point> points = getConnectedNeighbors(imageData, new Point(x,y), threshold);
+
+        for (Point p : points) {
+            output[(int)p.getX()][(int)p.getY()] = Color.WHITE.getRGB();
+        }
+
+        ImageIO.write(ImageIOUtils.getImageFromData(output), "png", new File(destPath));
     }
 
-    private static Set<Point> searchForConnectedNeighbors(int[][] img, Point startPoint, int threshold) {
+    public static Set<Point> getConnectedNeighbors(int[][] img, Point startPoint, int threshold) {
         Set<Point> connectedRegion = new HashSet<Point>();
         Set<Point> open = new HashSet<Point>();
         Set<Point> closed = new HashSet<Point>();
