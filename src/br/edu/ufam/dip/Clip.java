03ad83a8a6690789be419d45392a0f74527e51bb
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-06-18 23:56:39 -0400

    Added image clipping as standalone class
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

---
+++ src/br/edu/ufam/dip/Clip.java
@@ -0,0 +1,40 @@
+package br.edu.ufam.dip;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import br.edu.ufam.ImageIOUtils;
+
+public class Clip {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 6) {
+            System.err.println("Usage:    NegativeFilter <source-image> <dest-image> <x> <y> <width> <height>");
+            System.err.println("example:  NegativeFilter source.png dest.png 0 20 40 50");
+            System.exit(1);
+        }
+        String origPath = args[0];
+        String destPath = args[1];
+        int x = Integer.parseInt(args[2]);
+        int y = Integer.parseInt(args[3]);
+        int w = Integer.parseInt(args[4]);
+        int h = Integer.parseInt(args[5]);
+
+        BufferedImage image = ImageIOUtils.loadImageFromFile(origPath);
+        int[][] imageData = ImageIOUtils.getImageData(image);
+        int[][] output = clip(imageData, x, y, w, h);
+        ImageIO.write(ImageIOUtils.getImageFromData(output), "png", new File(destPath));
+    }
+
+    public static int[][] clip(int[][] image, int x, int y, int width, int height) {
+        int[][] output = new int[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                output[i][j] = image[x + i][y + j];
+            }
+        }
+        return output;
+    }
+
+}
